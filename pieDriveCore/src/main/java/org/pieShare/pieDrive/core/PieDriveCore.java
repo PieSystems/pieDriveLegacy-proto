a031412595575531235dadfa211d2468d6b564a8
Svetoslav Videnov <e1025844@student.tuwien.ac.at>
2015-11-13 11:46:50 +0100

    wrote base HashInputStream + started core interface

---
+++ pieDriveCore/src/main/java/org/pieShare/pieDrive/core/PieDriveCore.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pieShare.pieDrive.core;
+
+/**
+ *
+ * @author Svetoslav Videnov <s.videnov@dsg.tuwien.ac.at>
+ */
+public interface PieDriveCore {
+	void handleHash(byte[] hash);
+}
