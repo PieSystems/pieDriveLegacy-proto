16c894e156f9c9c3f2fe65abac389fcc991d2260
Svetoslav Videnov <e1025844@student.tuwien.ac.at>
2015-11-13 11:15:35 +0100

    created empty hashingInputStream

---
+++ pieDriveCore/src/main/java/org/pieShare/pieDrive/core/stream/HashingInputStream.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pieShare.pieDrive.core.stream;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ *
+ * @author Svetoslav Videnov <s.videnov@dsg.tuwien.ac.at>
+ */
+public class HashingInputStream extends InputStream {
+
+	@Override
+	public int read() throws IOException {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+	
+}
