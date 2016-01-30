5585e57a5673de7c76cf1408078912f101ddf7c9
Julian Pfanner <julian.pfanner@gmail.com>
2016-01-30 14:53:14 +0100

    started implementing recombining and recovery for raid 5

--- pieDriveCore/src/main/java/org/pieShare/pieDrive/core/ReedSolomonRaid5Service.java
+++ pieDriveCore/src/main/java/org/pieShare/pieDrive/core/ReedSolomonRaid5Service.java
@@ -1,12 +1,16 @@
 package org.pieShare.pieDrive.core;
 
 import com.backblaze.erasure.ReedSolomon;
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import org.pieShare.pieDrive.core.model.ChunkHealthState;
 import org.pieShare.pieDrive.core.model.PhysicalChunk;
 import org.pieShare.pieDrive.core.stream.BoundedInputStream;
+import org.pieShare.pieDrive.core.stream.BoundedOutputStream;
 import org.pieShare.pieDrive.core.stream.util.StreamFactory;
+import org.pieShare.pieTools.pieUtilities.service.pieLogger.PieLogger;
 
 public class ReedSolomonRaid5Service implements Raid5Service {
 	private AdapterCoreService adapterCoreService;
@@ -26,7 +30,11 @@ public class ReedSolomonRaid5Service implements Raid5Service {
 	}
 	
 	public int getDataShardCount() {
-		return adapterCoreService.getAdapters().size() - parityShardCount;
+		return getTotalShardCount() - parityShardCount;
+	}
+	
+	public int getTotalShardCount() {
+		return adapterCoreService.getAdapters().size();
 	}
 	
 	@Override
@@ -42,24 +50,64 @@ public class ReedSolomonRaid5Service implements Raid5Service {
 	@Override
 	public byte[][] generateRaidShards(InputStream inputStream, PhysicalChunk physicalChunk) {
 		int raidChunkSize = calculateRaidChunkSize(physicalChunk);
-		int adapterCount = adapterCoreService.getAdapters().size();
 
 		BoundedInputStream boundedStream = StreamFactory.getLimitingInputStream(inputStream, physicalChunk.getSize());
 
-		byte[][] raidBuffers = new byte[adapterCount][raidChunkSize];
+		byte[][] raidBuffers = new byte[getTotalShardCount()][raidChunkSize];
 		
 		for(int i = 0; i < getDataShardCount(); i++) {
 			try {
 				boundedStream.read(raidBuffers[i]);
 			} catch (IOException ex) {
-				//TODO error handling
+				PieLogger.error(this.getClass(), "Could not read buffer from input stream", ex);
 			}
 		}
 
-		ReedSolomon reedSolomon = ReedSolomon.create(adapterCount - parityShardCount, parityShardCount);
+		ReedSolomon reedSolomon = ReedSolomon.create(getDataShardCount(), getParityShardCount());
 		reedSolomon.encodeParity(raidBuffers, 0, raidChunkSize);
 		
 		return raidBuffers;
+	}
+
+	@Override
+	public void recombineRaidShards(OutputStream outputStream, PhysicalChunk physicalChunk, byte[][] shards) {
+		//TODO set stream offset or is this done at creation of the stream?
+		BoundedOutputStream boundedStream = StreamFactory.getBoundedOutputStream(outputStream, physicalChunk.getSize());
+		for(int i = 0; i < getDataShardCount(); i++) {
+			try {
+				boundedStream.write(shards[i]);
+			} catch (IOException ex) {
+				PieLogger.error(this.getClass(), "Could not write buffer to output stream", ex);
+				//TODO error handling
+			}
+		}
+	}
+
+	@Override
+	public boolean repairRaidShards(byte[][] shards, PhysicalChunk physicalChunk) {
+		boolean[] healthStates = new boolean[physicalChunk.getChunks().size()];
+		
+		//TODO map correct shard to adapter chunk
+		int brokenShardCount = 0;
+		for(int i = 0; i < physicalChunk.getChunks().size(); i++) {
+			if(physicalChunk.getChunks().get(i).getState() == ChunkHealthState.Healthy) {
+				healthStates[i] = true;
+			}
+			else {
+				healthStates[i] = false;
+				brokenShardCount++;
+			}
+		}
+		
+		if(brokenShardCount > getParityShardCount()) {
+			return false; // Shards cannot be repaired
+		}
+		
+		ReedSolomon reedSolomon = ReedSolomon.create(getDataShardCount(), getParityShardCount());
+		reedSolomon.decodeMissing(shards, healthStates, 0, calculateRaidChunkSize(physicalChunk));
+		
+		//TODO check if properly recovered and update physical chunk?
+		return true;
 	}
 
 }
